a9134f416f8ced23ebb068644e326194681bb2d1
ash_hyeong <chamchi0129@gmail.com>
2024-11-04 20:12:58 +0900

    up...date...utf8..plz..

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from data.schedule import *
 from collections import OrderedDict
 app = FastAPI()
@@ -8,10 +8,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    response = OrderedDict([
+    response_data = OrderedDict([
         ("다음 비래동에서 출발시간", next_birae_time()),
         ("이전 비래동에서 출발시간", previous_birae_time()),
         ("다음 판암역에서 출발시간", next_panam_time()),
         ("이전 판암역에서 출발시간", previous_panam_time())
     ])
-    return response
+    return Response(content=response_data, media_type="application/json;charset=utf-8")
